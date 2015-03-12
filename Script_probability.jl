5a78106760c80367d89df68f98a400350ae7ec1d
enara <enareando@gmail.com>
2015-03-12 19:22:48 +0100

    ya hace la probabilidad para un juego. Era un simple producto de matrices

--- Script_probability.jl
+++ Script_probability.jl
@@ -1,37 +1,24 @@
-module Estad
+module Stat
 
-sender= [399 1 1 1; 1 453 1 1; 1 1 324 1; 1 1 1 234]
-receiver=[399 1 1 1; 1 453 1 1; 1 1 324 1; 1 1 1 234]
+sender= [1 1 1 399; 1 1 453 1; 1 324 1 1; 456 1 1 1]
+receiver=[ 1 1 1 399; 1 1 453 1; 1 324 1 1; 234 1 1 1]
 matrix= Float64[] 
+num_games=1000
 
 #para sacar la probabilidad de una sola fila
-function prob_array(i)
-       array_s= sender[i , :]
-       prob_array_s= maximum (array_s)/sum (array_s)
 
-       ind=ind2sub(size(array_s), indmax(array_s))[2]#da la posicion del max
+function prob(sender)
+    sender/sum(sender)
+end
 
-       array_r= receiver[ind , :]
-       prob_array_r= array_r[1,i]/sum (array_r)
-      return 0.25*prob_array_s*prob_array_r  
+#para multiplicar matrices
+#function product(sender,receiver)
+    ms=mapslices(Stat.prob, sender ,2) 
+    mr=mapslices(Stat.prob, receiver ,2) 
+    probab=0.25*ms*mr
+#end
+
+
 end
 
 
-#para prob_game #pero me da 8 elementos en lugar de 4... hay que arreglarlo...me dice que matrix_game no está definido. 
-function prob_game(matrix)
-    for i=1:4
-        matrix_game=copy (matrix)
-        push!(matrix_game, Estad.prob_array(i))
-    end
-    println ("matrixgame: ", matrix_game)
-    return  mean(matrix_game)
-            std(matrix_game)
-end
-
-#hay que hacer la media y desviación meterlo en otro matriz donde estén todas los juegos(1000)
-#prob_total
-
-# hay que hacer la estadística de esta nueva matriz...
-
-
-end
